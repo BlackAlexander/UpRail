497ebd2dec0c53205cb654ce8150381b4e472a0e
BlackAlexander <acon3331@yahoo.com>
2024-06-03 18:35:08 +0300

    Respond to Pings

--- _tails/main.py
+++ _tails/main.py
@@ -1,3 +1,41 @@
-if __name__ == '__main__':
-    print('PyCharm')
-    
+import os
+import time
+import threading
+
+
+documents_path = os.path.join(os.path.expanduser('~'), 'Documents')
+ping_path = os.path.join(documents_path, 'upRail', 'PING.uprail')
+
+
+def respond_to_ping():
+    try:
+        with open(ping_path, 'r') as file:
+            content = file.read()
+
+        if content == '0':
+            content = '1'
+            with open(ping_path, 'w') as file:
+                file.write(content)
+    except FileNotFoundError:
+        print("PING_ERROR file_not_found")
+
+
+def monitor_ping():
+    while True:
+        respond_to_ping()
+        time.sleep(1)
+
+
+monitor_thread = threading.Thread(target=monitor_ping)
+monitor_thread.daemon = True
+monitor_thread.start()
+
+
+def main():
+    for i in range(10):
+        print(f"Main program is running... {i}")
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
