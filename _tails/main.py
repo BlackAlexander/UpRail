43cbbd5b0729263826f1bbb17ffa7c0b122f08d5
BlackAlexander <acon3331@yahoo.com>
2024-06-06 01:49:15 +0300

    Account for Environment file creation

--- _tails/main.py
+++ _tails/main.py
@@ -13,9 +13,21 @@ comm_thread = threading.Thread(target=monitor_comm)
 comm_thread.daemon = True
 comm_thread.start()
 
+documents_path = os.path.join(os.path.expanduser('~'), 'Documents')
+env_path = os.path.join(documents_path, 'upRail', 'Environment.uprail')
+
+
+def check_env():
+    env_content = "mock_sim_slope_diffusion: 0.3\ngravity_acceleration: 9.8\nlow_efficiency_obstruction: 1\n" \
+                  "low_efficiency_timer: 1000"
+    if not os.path.exists(env_path):
+        with open(env_path, 'w') as file:
+            file.write(env_content)
+
 
 def main():
     print("Server running...")
+    check_env()
     time.sleep(3600)
     restore_test_comm()
 
